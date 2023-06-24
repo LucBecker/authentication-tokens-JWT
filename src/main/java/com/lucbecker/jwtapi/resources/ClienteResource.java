7bd771a08f4b315f318c320c68d9f5bf5ccf21bf
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-24 00:40:24 -0300

    ClienteDTO implementado e ClienteResource iniciado

---
+++ src/main/java/com/lucbecker/jwtapi/resources/ClienteResource.java
@@ -0,0 +1,9 @@
+package com.lucbecker.jwtapi.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class ClienteResource {
+}
