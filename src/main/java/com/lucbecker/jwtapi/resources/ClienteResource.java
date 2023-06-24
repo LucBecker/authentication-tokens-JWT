bffbfc4298f530bf8bf99154c789a4ba0c32fd2e
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-24 00:45:39 -0300

    findAll para Cliente DTO

--- src/main/java/com/lucbecker/jwtapi/resources/ClienteResource.java
+++ src/main/java/com/lucbecker/jwtapi/resources/ClienteResource.java
@@ -1,6 +1,7 @@
 package com.lucbecker.jwtapi.resources;
 
 import com.lucbecker.jwtapi.domain.Cliente;
+import com.lucbecker.jwtapi.dtos.ClienteDTO;
 import com.lucbecker.jwtapi.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/usuarios")
@@ -20,5 +24,12 @@ public class ClienteResource {
     public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
         Cliente obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll() {
+        List<Cliente> list = service.findAll();
+        List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
