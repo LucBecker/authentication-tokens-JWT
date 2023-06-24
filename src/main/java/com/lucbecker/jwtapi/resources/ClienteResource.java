1e6701786536d3b6b8abf7dcea76807462d3e14f
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-24 00:48:26 -0300

    create para novo Cliente

--- src/main/java/com/lucbecker/jwtapi/resources/ClienteResource.java
+++ src/main/java/com/lucbecker/jwtapi/resources/ClienteResource.java
@@ -5,11 +5,11 @@ import com.lucbecker.jwtapi.dtos.ClienteDTO;
 import com.lucbecker.jwtapi.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,11 @@ public class ClienteResource {
         List<Cliente> list = service.findAll();
         List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    public ResponseEntity<Cliente> create(@Valid @RequestBody ClienteDTO objDto) {
+        Cliente newObj = service.create(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).body(newObj);
     }
 }
--- src/main/java/com/lucbecker/jwtapi/services/ClienteService.java
+++ src/main/java/com/lucbecker/jwtapi/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.lucbecker.jwtapi.services;
 
 import com.lucbecker.jwtapi.domain.Cliente;
+import com.lucbecker.jwtapi.dtos.ClienteDTO;
 import com.lucbecker.jwtapi.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,10 @@ public class ClienteService {
 
     public List<Cliente> findAll() {
         return repository.findAll();
+    }
+
+    public Cliente create(ClienteDTO objDTO) {
+        Cliente newObj = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getSenha());
+        return repository.save(newObj);
     }
 }
