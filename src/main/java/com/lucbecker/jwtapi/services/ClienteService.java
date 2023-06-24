465ccd988d66defff63a7a10d73b315774f3e7a2
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-24 00:42:56 -0300

    findById para Cliente implementado

---
+++ src/main/java/com/lucbecker/jwtapi/services/ClienteService.java
@@ -0,0 +1,20 @@
+package com.lucbecker.jwtapi.services;
+
+import com.lucbecker.jwtapi.domain.Cliente;
+import com.lucbecker.jwtapi.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    public Cliente findById(Integer id) {
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
